8192171b3dd3ecd5877b13f173837fcdae045fd6
DuniaIlmu <127168376+DuniaIlmu@users.noreply.github.com>
2023-03-19 17:47:07 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ocNm21sVPK":
+        Script1();
+        break;
+      case "6mKus0QRoit":
+        Script2();
+        break;
+      case "6cF4hGEiW9N":
+        Script3();
+        break;
+      case "6D6bGQ1XVuz":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script3()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script4()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
